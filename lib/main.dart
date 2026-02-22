a5df994d5a1ccce545bb668b8fa9b33d84bfb252
Tuhin Ikbal Eyamin <codereyamin@gmail.com>
2026-02-22 12:52:54 +0600

    bug fix deep link

--- lib/main.dart
+++ lib/main.dart
@@ -1,131 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:app_links/app_links.dart';
+import 'package:learning_deep_linking/screens/error_page.dart';
+import 'package:learning_deep_linking/screens/home_page.dart';
+import 'package:learning_deep_linking/screens/profile_page.dart';
+import 'package:learning_deep_linking/screens/splash_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Fetch initial link (cold start)
-  String? initialLink;
-  try {
-    initialLink = await AppLinks().getInitialLinkString();
-    print('Initial link (cold start): $initialLink');
-  } catch (e) {
-    print('Error fetching initial link: $e');
-  }
-
-  runApp(MyApp(initialLink: initialLink));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final String? initialLink;
-  const MyApp({super.key, this.initialLink});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Determine initial route based on deep link
-    String? initialRoute = '/';
-    if (initialLink != null) {
-      final uri = Uri.parse(initialLink!);
-      if (uri.pathSegments.isNotEmpty && uri.pathSegments[0] == 'number') {
-        initialRoute = '/number/${uri.pathSegments[1]}';
-      }
-    }
-
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: initialRoute,
+      initialRoute: '/',
+      unknownRoute: GetPage(name: '/error', page: () => ErrorPage()),
       getPages: [
-        GetPage(name: '/', page: () => HomePage()),
-        GetPage(name: '/number/:id', page: () => NumberPage()),
+        GetPage(name: '/', page: () => SplashPage()),
+        GetPage(name: '/home', page: () => HomePage()),
+        GetPage(name: '/profile', page: () => ProfilePage()),
+        GetPage(name: '/error', page: () => ErrorPage()),
       ],
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  final AppLinks _appLinks = AppLinks();
-
-  @override
-  void initState() {
-    super.initState();
-
-    // Listen for deep links when the app is running (foreground/background)
-    _appLinks.uriLinkStream.listen((uri) {
-      if (uri != null) {
-        print('Received deep link while running: $uri');
-        if (uri.pathSegments.isNotEmpty && uri.pathSegments[0] == 'number') {
-          final id = uri.pathSegments[1];
-          Get.toNamed('/number/$id');
-        }
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Home")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Waiting for deep link..."),
-            SizedBox(height: 20),
-            Text(
-              "Try: myapp://tometo/number/123",
-              style: TextStyle(fontSize: 12, color: Colors.grey),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class NumberPage extends StatefulWidget {
-  const NumberPage({super.key});
-
-  @override
-  State<NumberPage> createState() => _NumberPageState();
-}
-
-class _NumberPageState extends State<NumberPage> {
-    final AppLinks _appLinks = AppLinks();
-@override
-  void initState() {
-    super.initState();
-
-    // Listen for deep links when the app is running (foreground/background)
-    _appLinks.uriLinkStream.listen((uri) {
-      if (uri != null) {
-        print('Received deep link while running: $uri');
-        if (uri.pathSegments.isNotEmpty && uri.pathSegments[0] == 'number') {
-          final id = uri.pathSegments[1];
-          Get.toNamed('/number/$id');
-        }
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final id = Get.parameters['id'] ?? 'unknown';
-    return Scaffold(
-      appBar: AppBar(title: Text("Number Page")),
-      body: Center(
-        child: Text(
-          "Number from URL: $id",
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
     );
   }
 }
---
+++ lib/screens/splash_page.dart
@@ -0,0 +1,71 @@
+import 'dart:async';
+
+import 'package:app_links/app_links.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  final AppLinks _appLinks = AppLinks();
+  StreamSubscription? _sub;
+  bool _handledDeepLink = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initDeepLinks();
+  }
+
+  Future<void> _initDeepLinks() async {
+    try {
+      // Cold start
+      final uri = await _appLinks.getInitialLink();
+      if (uri != null) {
+        _handledDeepLink = true;
+        _navigateFromUri(uri);
+      }
+
+      // While app running
+      _sub = _appLinks.uriLinkStream.listen((uri) {
+        _navigateFromUri(uri);
+      });
+
+      // Normal splash delay
+      await Future.delayed(const Duration(seconds: 2));
+
+      if (!_handledDeepLink) {
+        Get.toNamed("/profile");
+      }
+    } catch (_) {
+      Get.toNamed("/error");
+    }
+  }
+
+  void _navigateFromUri(Uri uri) {
+    debugPrint("Deep Link Received: $uri");
+
+    final path = uri.path;
+
+    if (path.isNotEmpty && path != "/") {
+      Get.toNamed(path);
+      return;
+    }
+  }
+
+  @override
+  void dispose() {
+    _sub?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("Splash Screen")));
+  }
+}
